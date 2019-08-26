e1832defbc3dbb2ac844954cbf522a4221082928
Beth Ann <herbivore137@gmail.com>
2019-08-26 11:12:58 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,9 +30,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    # @user = User.find_by(email: params["email], password: params[:password])
     @user = User.find_by(email: params["email"], password: params["password"])
-    # session[:user_id] = @user.id 
    
     if @user
       session[:user_id] = @user.id
